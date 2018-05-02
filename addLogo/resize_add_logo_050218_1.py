03e0bf08d575cd221bf04048acb611883b825d1a
Sunny Lam <sunny.lam@gmail.com>
2018-05-02 11:05:01 -0400

    revised code

--- addLogo/resize_add_logo_050218_1.py
+++ addLogo/resize_add_logo_050218_1.py
@@ -14,37 +14,56 @@ from PIL import Image
 
 SQUARE_FIT_SIZE = 300
 LOGO_FILENAME = 'catlogo.png'
-target_dir = '.' # use '.' for current working directory (cwd)
+target_dir = '.' # use '.' for current working directory (cwd), default
+target_dir = "/Users/sunnyair/Dropbox/Python Programming/auto_bore_py_020518_1"
 output_dir = 'withLogo'
+output_dir = "/Users/sunnyair/Downloads/withLogo"
 
 logoIm = Image.open(LOGO_FILENAME)
 logoWidth,logoHeight = logoIm.size # `logoIm.size` returns a tuple of width, height
 
 os.makedirs(output_dir,exist_ok=True) # create a folder unless it already exists
 
+# change current working directory if not using the default
+
+if target_dir != ".":
+	os.chdir(target_dir)
+
 # loop over all files in the working directory
 
+# logging.debug( 'Files in target directory are:  %s' % str( os.listdir(target_dir) ) )
+
 for filename in os.listdir(target_dir):
+
 	if not ( filename.endswith('.png') or filename.endswith('.jpg') or filename == LOGO_FILENAME ):
 		continue # skip non-image files and the logo file itself
 
+	logging.debug( 'Accessing file:  %s' % str(filename) )
+
+	logging.debug( 'Opening file:  %s' % str(filename) )
 	im = Image.open(filename)
+	logging.debug( 'Opening file successful:  %s' % str(filename) )
 	width,height = im.size
+	logging.debug( 'Width and height of image are:  W %d , H %d' % (width,height) )
 
-# check if the image needs to be resized
+	# check if the image needs to be resized
 
-if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
-	# calculate the new width and height to resize to
-	if width > height:
-		height = int( (SQUARE_FIT_SIZE/width) * height )
-		width = SQUARE_FIT_SIZE
-	else:
-		width = int( (SQUARE_FIT_SIZE/height) * width )
-		height = SQUARE_FIT_SIZE
+	if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
+		logging.debug( 'Image exceeds our fit size' )
+		# calculate the new width and height to resize to
+		if width > height:
+			logging.debug( 'Width is greater than height.' )
+			height = int( (SQUARE_FIT_SIZE/width) * height )
+			width = SQUARE_FIT_SIZE
+		else:
+			logging.debug( 'Height is greater than width.' )
+			width = int( (SQUARE_FIT_SIZE/height) * width )
+			height = SQUARE_FIT_SIZE
 
-	# resize the image
-	print('Resizing %s...' % str(filename))
-	im = im.resize( (width,height) )
+		# resize the image
+		print('Resizing %s...' % str(filename))
+		im = im.resize( (width,height) )
+		logging.debug( 'Resizing image successful.' )
 
 # add the logo
 # save changes
